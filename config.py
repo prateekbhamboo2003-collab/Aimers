c19654a52eb1be22dc93ecd0f24ee3f6a71f3c2f
prateekbhamboo2003-collab <prateekbhamboo2003@gmail.com>
2025-11-18 13:24:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-API_ID = API_ID =  
+API_ID = API_ID = 32402877 
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "145427eddb5c1bc4dc943d0a89f0f98b")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8458145401:AAGVWdj2-kWFk6oVSDrY_0IFk1XNzN5T7Fc")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
@@ -14,7 +14,7 @@ LOG = ,
 
 # UPDATE_GRP = , # bot sat group
 
-# auth_chats = []
+# auth_chats = []100179526608
 
 try:
     ADMINS=[]
